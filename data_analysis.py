e193ddce2b8c18ce065652cc9e14c211a8eb1c52
same19 <saengel123@gmail.com>
2023-07-31 14:59:06 -0700

    Process data updates

--- data_analysis.py
+++ data_analysis.py
@@ -2,29 +2,50 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-data = [] #time: intensity
+#time, intensity, intensityRefA, intensityRefB
+data = [
+    [0,1,2,3,4,5,6,7,8],
+    [1, 1, 0.5, 0.7, 0, 2.0, 3.0, 2.0, 2.0],
+    [0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7,0.7],
+    [0.95,0.95,0.95,0.95,0.95,0.95,0.95,0.95,0.95]
+]
 wavelength = 780 #nanometers
+
+def retrieveData():
+    return []
 
 def processData(data):
     time = [i[0] for i in data]
     intensity = [i[1] for i in data]
-    phase = np.multiply(2, np.arccos(np.sqrt(np.multiply(0.25, intensity))))
+    A = [i[2] for i in data]
+    B = [i[3] for i in data]
+    phase = np.arccos(
+        np.multiply(
+            np.subtract(
+                np.subtract(intensity, A),
+                B
+            ),
+            np.power(
+                np.multiply(
+                    2,
+                    np.multiply(A,B)
+                ),
+                -1
+            )
+        )
+    )
     d = np.multiply(phase, wavelength / (2 * np.pi))
     """
-    phase = 0: intensity = 2*single
-    phase = 90: 
-    phase = 180:
-    sina + sinb = 2sin((a+b)/2)cos((a-b)/2)
-    -> sin(x) + sin(x+theta) = 2sin(x+theta/2)cos(theta/2)
-    -> amplitude of new wave is 2cos(theta/2)
-    -> intensity of new wave is 4cos^2(theta/2)
-    -> if we have the new wave's intensity J, and the base wave's intensity I, then the phase shift is 2 * arccos(sqrt(J/4)) which will be in 0 to pi radians
+    amp = sqrt(a^2 + b^2 + 2abcos(phase shift))
+    phase shift = arccos((I - A - B)/(2ab))
     
     """
     return [time, d]
 
 def main():
-    return
+    processData(data)
+    plt.plot(data[0], data[1])
+    plt.show()
 
 
 if __name__=="__main__":
