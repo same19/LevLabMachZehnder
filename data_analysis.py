1096b54adff3cf8e2248ff933c8e68ea4117736d
same19 <saengel123@gmail.com>
2023-08-04 14:24:24 -0700

    Edits for real data

--- data_analysis.py
+++ data_analysis.py
@@ -5,33 +5,35 @@ import os
 import csv
 
 wavelength = 780 #nanometers
-minV =  1.8#volts
-maxV =  2.2#volts
+filename = "TEK00011.CSV"
 
 def retrieveData():
+    global sample_rate_inverse
     data = []
     directory = "raw_data"
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        with open(f) as csvfile:
-            rows = list(csv.reader(csvfile))
-            start = 16
-            num = int(rows[8][1])
-            data = [[float(item[i]) for item in rows[start:start + num]] for i in range(5)]
-        break
+    f = os.path.join(directory, filename)
+    with open(f) as csvfile:
+        rows = list(csv.reader(csvfile))
+        start = 16
+        num = int(rows[8][1])
+        sample_rate_inverse = float(rows[6][1])
+        data = [[float(item[i]) for item in rows[start:start + num]] for i in range(2)]
     return data
 
 def processData(data):
-    time = [i[0] for i in data]
+    time = data[0]
     # intensity = [i[1] for i in data]
-    A = [i[1] for i in data]
-    B = [i[2] for i in data]
+    A = data[1]
+    
+    minV =  np.min(A)#volts
+    maxV =  np.max(A)#volts
+    print(minV)
+    print(maxV)
+
+    # B = [i[2] for i in data]
     phase = np.arccos(
         np.multiply(
-            np.subtract(
-                A,
-                B
-            ),
+            A,
             1/(maxV-minV)
         )
     )
@@ -42,6 +44,16 @@ def main():
     data = retrieveData()
     newData = processData(data)
     plt.plot(newData[0], newData[1])
+    plt.show()
+    fr = np.fft.rfftfreq(len(newData[1]),d=sample_rate_inverse)
+    start_index = 1
+    fr = fr[start_index:]
+    amp = np.multiply(np.abs(np.fft.rfft(newData[1])), 2./len(newData[1]))[start_index:]
+    ampFit = np.multiply(np.power(fr, -1),42.4044555918)
+    plt.plot(fr, amp)
+    plt.plot(fr, ampFit)
+    plt.show()
+    plt.plot(fr, np.subtract(amp,ampFit))
     plt.show()
 
 
